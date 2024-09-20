f35a269676a5abbcf6b96186896af614b0e1d812
KeeganM-02 <keegan.miller@hotmail.com>
2024-09-20 10:51:38 -0400

    Terminal user input created
    -Deleted unnecessary data files from file
    -Commented out code used to print data while training model
    -Implemented very simple, terminal-based user input to predict a given grade

--- Grade Predictor.py
+++ Grade Predictor.py
@@ -4,6 +4,7 @@ import sklearn
 from sklearn import linear_model
 import matplotlib.pyplot as pyplot
 import pickle
+from tkinter import *
 
 
 ##Create dataframe using student-mat.csv
@@ -38,10 +39,41 @@ model = pickle.load(pickle_in)
 
 score = model.score(x_test,y_test)
 
-print(score)
-print("Coef: ", model.coef_)
-print("Intercept: ", model.intercept_)
+####### Used this code to show the score, coefficients, and y-intercepts of the model being trained, as well as output it's predictions #######
+# print(score)
+# print("Coef: ", model.coef_)
+# print("Intercept: ", model.intercept_)
+#
+# predictions = model.predict(x_test)
+# for x in range(len(predictions)):
+#     print(predictions[x], x_test[x], y_test[x])
 
-predictions = model.predict(x_test)
-for x in range(len(predictions)):
-    print(predictions[x], x_test[x], y_test[x])
+
+
+##Get user inputs from terminal
+print("first period score (1-20): ")
+userPer1 = int(input())
+print("second period score (1-20): ")
+userPer2 = int(input())
+print("age (in years): ")
+userAge = int(input())
+print("mother's education level (0: none, 1: primary education (4th grade), 2: 5th to 9th grade, 3: secondary education or 4: higher education): ")
+userMedu = int(input())
+print("father's education level (0: none, 1: primary education (4th grade), 2: 5th to 9th grade, 3: secondary education or 4: higher education): ")
+userFedu = int(input())
+print("weekly study time (1: <2 hours, 2: 2 to 5 hours, 3: 5 to 10 hours, or 4: >10 hours): ")
+userStudy = int(input())
+print("number of past class failures (0-4): ")
+userFail = int(input())
+print("quality of family relations (1: very bad, 5: excellent): ")
+userFamrel = int(input())
+print("weekend alcohol consumption (1: very low, 5: very high): ")
+userWalc = int(input())
+print("current health status (1: very bad, 5: very good): ")
+userHealth = int(input())
+
+userInputs=[[userPer1,userPer2,userAge,userMedu,userFedu,userStudy,userFail,userFamrel,userWalc,userHealth]]
+
+prediction = model.predict(userInputs)
+print("Predicted final score (from 1-20): ", prediction[0])
+
