2ba83b18fcaf20a5304762a0460b46b21a909b51
KeeganM-02 <keegan.miller@hotmail.com>
2024-09-20 19:56:32 -0400

    Created GUI
    -Removed code for terminal-based user input
    -Created GUI to receive user input, and output predicted G3 grade

--- Grade Predictor.py
+++ Grade Predictor.py
@@ -6,6 +6,13 @@ import matplotlib.pyplot as pyplot
 import pickle
 from tkinter import *
 
+def Predict():
+    userInputs = [[int(Input_G1.get()),int(Input_G2.get()),int(Input_Age.get()),int(Input_Medu.get()),int(Input_Fedu.get()),int(Input_Study.get()),int(Input_Fail.get()),
+                   int(Input_Famrel.get()),int(Input_Walc.get()),int(Input_Health.get())]]
+    prediction = model.predict(userInputs)
+    Output_text.insert(END, round(prediction[0],2))
+
+
 
 ##Create dataframe using student-mat.csv
 df = pd.read_csv("student-mat.csv",sep=';')
@@ -16,7 +23,8 @@ X = np.array(df.drop(["G3"],axis = 1))
 Y = np.array(df[valPredict])
 x_train,x_test,y_train,y_test = sklearn.model_selection.train_test_split(X,Y, test_size = 0.2)
 
-##Previously used loop to save the model with the best score into 'gradingmodel.pickle' to be used later.
+##Previously used loop to save the model with the best score into 'gradingmodel.pickle' to be used later.##
+####################If you would like to retrain the model, uncomment following loop and run as many iterations as you'd like!####################
 
 # best = 0
 # for i in range(1000):
@@ -49,31 +57,65 @@ score = model.score(x_test,y_test)
 #     print(predictions[x], x_test[x], y_test[x])
 
 
+###Create tkinter object
+root = Tk()
+root['bg'] = 'forest green'
+root.geometry('1100x500')
+root.resizable(True,True)
+root.title("Grade Predictor")
 
-##Get user inputs from terminal
-print("first period score (1-20): ")
-userPer1 = int(input())
-print("second period score (1-20): ")
-userPer2 = int(input())
-print("age (in years): ")
-userAge = int(input())
-print("mother's education level (0: none, 1: primary education (4th grade), 2: 5th to 9th grade, 3: secondary education or 4: higher education): ")
-userMedu = int(input())
-print("father's education level (0: none, 1: primary education (4th grade), 2: 5th to 9th grade, 3: secondary education or 4: higher education): ")
-userFedu = int(input())
-print("weekly study time (1: <2 hours, 2: 2 to 5 hours, 3: 5 to 10 hours, or 4: >10 hours): ")
-userStudy = int(input())
-print("number of past class failures (0-4): ")
-userFail = int(input())
-print("quality of family relations (1: very bad, 5: excellent): ")
-userFamrel = int(input())
-print("weekend alcohol consumption (1: very low, 5: very high): ")
-userWalc = int(input())
-print("current health status (1: very bad, 5: very good): ")
-userHealth = int(input())
+##General labels
+Label(root, text="Grade Predictor", font='Arial 20 bold', bg='light green').pack()
+Label(root, text="Input Your Information Here", font='Arial 15 bold', bg='light green').place(x=165,y=90)
+Label(root, text="Predicted Period 3 Grade", font='Arial 15 bold', bg='light green').place(x=715, y = 90)
 
-userInputs=[[userPer1,userPer2,userAge,userMedu,userFedu,userStudy,userFail,userFamrel,userWalc,userHealth]]
+##Label pointing to student.txt for more information on variables
+Label(root, text = "*Go to student.txt for more\n "
+                   "information on variables and scoring system*",
+      font = 'Arial 10 bold italic', bg='forest green').place(x=0,y=0)
 
-prediction = model.predict(userInputs)
-print("Predicted final score (from 1-20): ", prediction[0])
+##Labels for each variable
+Label(root, text="first period score (1-20)", font = 'Arial 10 bold', bg='light green').place(x=70, y = 145)
+Label(root, text="second period score (1-20)", font = 'Arial 10 bold', bg='light green').place(x=350, y = 145)
+Label(root, text="mother's education level (1-4)", font = 'Arial 10 bold', bg='light green').place(x=70, y = 215)
+Label(root, text="father's education level (1-4)", font = 'Arial 10 bold', bg='light green').place(x=350, y = 215)
+Label(root, text="weekly study time (1-4)", font = 'Arial 10 bold', bg='light green').place(x=70, y=285)
+Label(root, text="number of past class failures (0-4)", font = 'Arial 10 bold', bg='light green').place(x=350, y=285)
+Label(root, text="quality of family relations (1-5)", font = 'Arial 10 bold', bg='light green').place(x=70, y=355)
+Label(root, text="weekend alcohol consumption (1-5)", font = 'Arial 10 bold', bg='light green').place(x=350, y=355)
+Label(root, text="current health status (1-5)", font = 'Arial 10 bold', bg='light green').place(x=70, y=425)
+Label(root, text="current age (15-22)", font = 'Arial 10 bold', bg='light green').place(x=350, y=425)
+
+
+##Input boxes for each variable
+Input_G1 = Entry(root, border = 3, width = 23)
+Input_G1.place(x = 70, y=170)
+Input_G2 = Entry(root, border=3, width=23)
+Input_G2.place(x = 350, y = 170)
+Input_Medu = Entry(root, border = 3, width = 23)
+Input_Medu.place(x = 70, y = 240)
+Input_Fedu = Entry(root, border = 3, width = 23)
+Input_Fedu.place(x = 350, y = 240)
+Input_Study = Entry(root, border = 3, width = 23)
+Input_Study.place(x = 70, y = 310)
+Input_Fail = Entry(root, border = 3, width = 23)
+Input_Fail.place(x = 350, y = 310)
+Input_Famrel = Entry(root, border = 3, width = 23)
+Input_Famrel.place(x = 70, y = 380)
+Input_Walc = Entry(root, border = 3, width = 23)
+Input_Walc.place(x = 350, y = 380)
+Input_Health = Entry(root, border = 3, width = 23)
+Input_Health.place(x = 70, y = 450)
+Input_Age = Entry(root, border = 3, width = 23)
+Input_Age.place(x=350, y=450)
+
+
+Output_text = Text(root, font='arial 10', height=5, wrap=WORD, padx=5, pady=5, border = 3, width=33)
+Output_text.place(x=715, y=130)
+
+button = Button(root, text = "Generate Prediction", font = "arial 13 bold", command=Predict)
+button.place(x=750, y = 250)
+
+
+root.mainloop()
 
